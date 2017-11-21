46d6194cb8cf33bcf01606d969af0eb29d223abb
adganong <adam.b.ganong@gmail.com>
2017-11-20 21:43:10 -0400

    Now able to move. Jump is busted.

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -1,14 +1,12 @@
 ﻿using UnityEngine;
 
 public class Player_Controller : MonoBehaviour {
-
-    //declare the rigid body 2d we will be using later
-    private Rigidbody2D thisCharactersRigidbody;
-    private float speed = 3;
+    //declerations
+    public float playerSpeed    = 3f;
+    public float jumpForce      = 1250f;
 
 	// Use this for initialization
 	void Start () {
-        //thisCharactersRigidbody = GameObject.GetComponent<Rigidbody2D>();
     }
 	
 	//Update is called once per frame update
@@ -19,33 +17,22 @@ public class Player_Controller : MonoBehaviour {
 
     //this is called once before Physics is determined (all physics calculations should be done here)
     private void FixedUpdate() {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        var moveX = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        if (jump()){
+            moveUp();
+        }
     }
 
-   /* //grab inputs on the keyboard and returns the float value
-    private float checkForMovementInput() {
-        w("Inside check for movement");
-        return Input.GetAxis("Horizontal");
+    void moveUp(){
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
     }
-
-    //takes a Vector2 and moves the character based on the values
-    private void MoveCharacter(Vector2 movement) {
-        w("Inside Move Character");
-        thisCharactersRigidbody.AddForce(movement);
-    }
-
-    //creates and returns a vector2 based on x and y values (y not needed) and returns them
-    private Vector2 createVector2For(float x, float y = 0) {
-        w("Inside Create Vector 2");
-        return new Vector2(x, y);
-    }*/
-
     void w(string toPrint) {
         Debug.Log(toPrint);
+    }
+
+    public bool jump(){
+        return Input.GetKeyDown(KeyCode.W);
     }
 
 
