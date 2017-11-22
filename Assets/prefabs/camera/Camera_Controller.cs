3a91583355206e123485a88a0eb8de5d80694873
Adam <adam.b.ganong@gmail.com>
2017-11-21 23:58:57 -0400

    camera system modified. Everything for the camera is inside a prefab now which is BAD but it is just for the prototype (AND so that it can be moved to other projects)

---
+++ Assets/prefabs/camera/Camera_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+    //to be able to grab easier
+    public GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    // Use this for initialization
+    void Start() {
+        //player = GameObject.FindGameObjectWithTag("Guy");
+    }
+
+    // LateUpdate is called once at the end of each frame
+    void LateUpdate() {
+        //grab thye x and y positions of the player
+        float xPos = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float yPos = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(xPos, yPos, gameObject.transform.position.z);
+    }
+}
