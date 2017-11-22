3b6f88094105d06dc4fb7ed42b02fd8b71d95534
Adam <adam.b.ganong@gmail.com>
2017-11-21 23:44:10 -0400

    Movement script has been changed to basic premise from tutorial. Jumping glitch no longer happens (I assumje do to the addition of a bool to ensure that the function is no being called multiple times) Impact controls fixed

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -1,7 +1,63 @@
 ﻿using UnityEngine;
 
 public class Player_Controller : MonoBehaviour {
-    //declerations
+    //initilzation
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public float playerJumpPower = 1250;
+    public float moveX;
+    public int jumpcount = 2;
+    public bool isGrounded;
+
+
+    // Update is called once per frame
+    void Update() {
+        PlayerMove();
+    }
+
+    void PlayerMove() {
+        bool testIfRunning;
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        Debug.Log(moveX);
+        if (Input.GetButtonDown("Jump") && jumpcount < 2) {
+            jumpcount++;
+            Jump();
+        }
+
+        //test if running and if it is send the proper valeu to is running
+        testIfRunning = moveX != 0 ? true : false;
+        //GetComponent<Animator>().SetBool("isRunning", testIfRunning);
+        //Animations
+        //Player Direction
+        if (moveX > 0.0f) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        } else if (moveX < 0.0f) {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void resetJump() {
+        jumpcount = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //how to check the name of what was hit. This is not useful
+        //if (collision.collider.name == "Ground") {
+        resetJump();
+    }
+
+    void OnTriggerEnter2D(Collider2D trig) {
+        Debug.Log("Has hit the end of the level");
+    }
+    //For science!
+    /*//declerations
     public float playerSpeed    = 3f;
     public float jumpForce      = 1250f;
 
@@ -33,7 +89,7 @@ public class Player_Controller : MonoBehaviour {
 
     public bool jump(){
         return Input.GetKeyDown(KeyCode.W);
-    }
+    }*/
 
 
 }
